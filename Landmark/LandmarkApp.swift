66a407e738fcff009fd2cc422255a81851a49589
Ali Parandeh <Ali.Parandeh@outlook.com>
2020-12-12 18:00:03 +0000

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Ali Parandeh on 12/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
